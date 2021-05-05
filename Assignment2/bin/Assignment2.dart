08e5271315d25705cc2ff8befe88fe5f57970b7d
Rachit Garg <rachitgarg900@gmail.com>
2021-05-05 21:25:36 +0200

    Added Assignment 2

--- Assignment2/bin/Assignment2.dart
+++ Assignment2/bin/Assignment2.dart
@@ -1,5 +1,31 @@
-import 'package:Assignment2/Assignment2.dart' as Assignment2;
+import 'dart:async';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${Assignment2.calculate()}!');
+void main(List<String> arguments) async {
+
+  print('Starting');
+
+  File f = await appendFile();
+
+  print('Appended!');
+
+  String content = await readFile(f);
+
+  print('Read!\r\n');
+
+  print('Content:');
+  print(content);
+
+  print('**** End ****');
+}
+
+Future<File> appendFile() {
+  File f = File(Directory.current.path + '/MyFile.txt');
+  print('Appending');
+  return f.writeAsString('Hello!\r\n', mode: FileMode.append);
+}
+
+Future<String> readFile(File f) {
+  print('Reading');
+  return f.readAsString();
 }
