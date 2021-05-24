136257da5a152049e547f64e680e01a1f1da8589
Rachit Garg <rachitgarg900@gmail.com>
2021-05-24 19:50:52 +0200

    added key derivation example

---
+++ Encryption_2/bin/Encryption_2.dart
@@ -0,0 +1,33 @@
+import 'dart:typed_data';
+import 'dart:convert';
+import 'package:pointycastle/pointycastle.dart';
+
+void main(List<String> arguments) {
+
+  String password = 'password';
+
+  //add salt to randomize algorithm
+  var salt = createUint8ListFromString('salT');
+
+  //key derivation algorithm
+  var pkcs = new KeyDerivator('SHA-1/HMAC/PBKDF2');
+
+  //parameters for the algorithm
+  var params = new Pbkdf2Parameters(salt, 100, 16);
+
+  //initialize algo with above parameters
+  pkcs.init(params);
+
+  //process password using the algorithm
+  var value = pkcs.process(createUint8ListFromString(password));
+  display('Key value', value);
+
+}
+//Function to directly create Uint8List from string for further processing
+Uint8List createUint8ListFromString(String string) => Uint8List.fromList(Utf8Codec().encode(string));
+
+void display(String title, Uint8List value) {
+  print('Title: ${title}');
+  print('Value: ${value}');
+  print('Value in a readable form: ${Base64Codec().encode(value)}');
+}
